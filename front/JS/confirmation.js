0f0e66b9dc10f1be901aa834f9342e4df7f7cbd9
Aeskender99 <alexandre.leonard.pro@gmail.com>
2022-10-25 09:16:20 +0200

    correction probleme ID

--- front/JS/confirmation.js
+++ front/JS/confirmation.js
@@ -2,7 +2,6 @@
 let href = window.location.href;
 let url = new URL(href);
 let searchId = new URLSearchParams(url.search);
-let id = 0;
 
 //recherche de l'id de commande dans l'url
 if(searchId.has("id")) {
